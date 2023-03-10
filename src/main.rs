011dcb4c695cf8859e27193339b204d4ec116e01
Lian <liannsd3@gmail.com>
2023-03-10 12:32:30 -0300

    b is for basic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 //logging
 use log::{debug, error, info, trace, warn, LevelFilter, SetLoggerError};
 
+//std
+use std::{error::Error, io::{self, Write, Read}, fs::File};
+
+//tui
+use tui::{
+    backend::{Backend, CrosstermBackend},Terminal
+};
+
 
 
 use crossterm:: {
@@ -13,32 +21,21 @@ use crossterm:: {
     }
 };
 
-use std::{error::Error, io::{self, Write, Read}, fs::File};
-use tui::{
-    backend::{Backend, CrosstermBackend},
-    layout::{Constraint, Direction, Layout, Alignment},
-    style::{Color, Modifier, Style},
-    text::{Span, Spans, Text},
-    widgets::{Block, Borders, List, ListItem, Paragraph, Wrap},
-    Frame, Terminal,
-};
-
-use unicode_width::UnicodeWidthStr;
 
 use app::{App, InputMode, MessageType};
 
 mod logging;
 mod api;
 mod app;
+mod render;
 
 
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>>{
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
 
     
     //setup logging
-    //*****************LOGGING************************************
 
     let log_file = "./log/logfile";
     let request_file = "./log/requests";
@@ -47,7 +44,6 @@ async fn main() -> Result<(), Box<dyn Error>>{
     let _handle = logging::set_logging(log_file, request_file);
 
 
-    //*****************LOGGING************************************
     //setup terminal
     enable_raw_mode()?;
     let mut stdout = io::stdout();
@@ -98,7 +94,7 @@ async fn run_app<B: Backend>(terminal : &mut Terminal<B>, mut app: App) -> io::R
     // let mut file = File::create("request.txt")?;
     loop {
 
-        terminal.draw(|f| ui(f, &app))?;
+        terminal.draw(|f| render::ui(f, &app))?;
 
         match event::read()? {
             Event::Resize(_, _) => {
@@ -108,6 +104,9 @@ async fn run_app<B: Backend>(terminal : &mut Terminal<B>, mut app: App) -> io::R
 
                 match app.input_mode() {
                     InputMode::Normal => match key.code {
+                        KeyCode::Char(':') => {
+                            app.set_input_mode(InputMode::Command);
+                        }
                         KeyCode::Char('i') => {
                             app.set_input_mode(InputMode::Insert);
                         }
@@ -131,13 +130,13 @@ async fn run_app<B: Backend>(terminal : &mut Terminal<B>, mut app: App) -> io::R
                                     app.get_display_input().drain(..).collect()
                                 );
 
-                                terminal.draw(|f| ui(f, &app))?;
+                                terminal.draw(|f| render::ui(f, &app))?;
                                 app.update_input();
-                                terminal.draw(|f| ui(f, &app))?;
+                                terminal.draw(|f| render::ui(f, &app))?;
 
                                 app.answer().await;
 
-                                let thing = app.get_call();
+                                // let thing = app.get_call();
 
                                 // file.write_all(format!("{:#?}",thing).as_bytes())?;
 
@@ -156,7 +155,25 @@ async fn run_app<B: Backend>(terminal : &mut Terminal<B>, mut app: App) -> io::R
                         }
 
                     }
-                    _ => {}
+                    InputMode::Command => {
+                        match key.code {
+
+                            KeyCode::Enter => {
+                                app.send_command();
+                            }
+                            KeyCode::Char(c) => {
+                                app.push_command(c);
+                            }
+                            KeyCode::Backspace => {
+                                app.pop_command();
+                            }
+                            KeyCode::Esc => {
+                                app.reset_command();
+                                app.set_input_mode(InputMode::Normal);
+                            }
+                            _ => {}
+                        }
+                    }
                 }
 
             }
@@ -165,88 +182,6 @@ async fn run_app<B: Backend>(terminal : &mut Terminal<B>, mut app: App) -> io::R
     }
 }
 
-fn ui<B: Backend> (f: &mut Frame<B>, app: &App) {
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(1)
-        .constraints([
-            Constraint::Percentage(10),
-            Constraint::Percentage(80),
-            Constraint::Percentage(10),
-        ].as_ref()
-    )
-    .split(f.size());
-
-
-
-
-    //TODO support changing models
-    let (msg, style) = 
-        (
-            vec![
-                Span::styled(" Davinci ",Style::default()
-                    .bg(Color::White)
-                    .fg(Color::Black)),
-                Span::raw(" Mark "),
-                Span::raw(" Dall-e "),
-                Span::raw(" Curie "),
-                Span::raw(" Ada "),
-                // Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
-                // Span::raw(" to exit, "),
-                // Span::styled("e", Style::default().add_modifier(Modifier::BOLD)),
-                // Span::raw(" to start editing."),
-            ],
-            Style::default().add_modifier(Modifier::RAPID_BLINK),
-       );
-
-
-    let mut text = Text::from(Spans::from(msg));
-    text.patch_style(style);
-    let help_message = Paragraph::new(text).block(Block::default()
-        .borders(Borders::TOP)
-        .title_alignment(Alignment::Center)
-        .title("  models  "))
-        .alignment(Alignment::Center);
-    f.render_widget(help_message, chunks[0]);
-
-
-    let input = Paragraph::new(app.get_display_input())
-        .style(match app.input_mode() {
-            InputMode::Normal => Style::default(),
-            InputMode::Insert => Style::default().fg(Color::Blue),
-            InputMode::Command => Style::default().fg(Color::Yellow),
-    })
-    .block(Block::default().borders(Borders::TOP).title("Query")).wrap(Wrap{trim: false});
-    f.render_widget(input, chunks[2]);
-
-    match app.input_mode() {
-        InputMode::Normal => {} //Hide the cursor. 'Frame does this by default.
-        InputMode::Insert => {
-            f.set_cursor(
-                chunks[2].x + app.get_display_input().width() as u16,
-                chunks[2].y + 1,
-            )
-        }
-        InputMode::Command => {
-            f.set_cursor(
-                chunks[2].x + app.get_display_input().width() as u16,
-                chunks[2].y + 1,
-            )
-        }
-
-    }
-    let messages: Vec<Spans> = app
-        .get_content();
-
-    let messages = 
-        Paragraph::new(messages).block(Block::default()
-        .borders(Borders::TOP)
-        .title("chat"))
-        .alignment(Alignment::Left)
-        .wrap(Wrap{trim: false})
-        .scroll((app.get_scroll() as u16,0));
-    f.render_widget(messages,chunks[1]);
-}
 
 
 
